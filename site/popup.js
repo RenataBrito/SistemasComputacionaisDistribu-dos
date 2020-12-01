695981bf9e9a70b743a7d6da15842ba1b688bd01
Renata_Brito <34607807+RenataBrito@users.noreply.github.com>
2020-11-30 23:26:39 -0300

    add novas paginas

---
+++ site/popup.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    // result tem que ser att e passar como parametro pra saber se a compra foi efetivada ou n
+    var result = "Sucesso";
+    alert(result);
+    window.location.href = "pergunta.html";
+}
+
+function yesFunction(){
+    window.location.href = "catalogo.html";
+}
+
+function noFunction(){
+    window.location.href = "final.html";
+}
